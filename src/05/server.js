9d34375219eb37f17d3fb2400a1d54dd94d5403c
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-03-04 00:11:54 +0200

    feat: updated 05 lesson

--- src/05/server.js
+++ src/05/server.js
@@ -1,31 +1,43 @@
 const http = require('http');
+const fs = require('fs');
 
 let requestCount = 0;
 
-const sever = http.createServer((req, res) => {
-    console.log(req);
-
-    if (req.url !== '/favicon.ico') {
+const server = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') {
+        // Read the favicon from file
+        fs.readFile('favicon.ico', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end();
+            } else {
+                // Send the favicon as response
+                res.writeHead(200, {'Content-Type': 'image/x-icon'});
+                res.end(data);
+            }
+        });
+    } else {
+        // Handle other requests
         requestCount++;
-    }
 
-    switch (req.url) {
-        case '/':
-        case '/nastya': {
-            res.write('Nastya');
-            break;
+        switch (req.url) {
+            case '/':
+            case '/nastya': {
+                res.write('Nastya');
+                break;
+            }
+            case '/vova': {
+                res.write('Vova');
+                break;
+            }
+            default: {
+                res.write('404 Not Found');
+            }
         }
-        case '/vova': {
-            res.write('Vova');
-            break;
-        }
-        default: {
-            res.write('404 Not Found');
-        }
-    }
 
-    res.write(' ' + requestCount);
-    res.end();
+        res.write(' ' + requestCount);
+        res.end();
+    }
 });
 
-sever.listen(3003);
+server.listen(3003);
