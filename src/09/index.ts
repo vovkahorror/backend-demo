058dd87a259b0e6ef1c0a9722f8907be4537e61a
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-03-12 02:10:55 +0200

    feat: updated 09 lesson

--- src/09/index.ts
+++ src/09/index.ts
@@ -1,9 +1,35 @@
 import express from 'express';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send({message: 'Hello'});
+});
+
+app.get('/courses', (req, res) => {
+    res.json([
+        {id: 1, name: 'front-end'},
+        {id: 2, name: 'back-end'},
+        {id: 3, name: 'full-stack'},
+        {id: 4, name: 'mobile'},
+    ]);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = [
+        {id: 1, name: 'front-end'},
+        {id: 2, name: 'back-end'},
+        {id: 3, name: 'full-stack'},
+        {id: 4, name: 'mobile'},
+    ].find(course => course.id === +req.params.id);
+
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.json(foundCourse);
 });
 
 app.listen(port, () => {
