9e55ddf8edfceb63d9aefede48d9ff2eac9b96ea
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-03-13 02:31:10 +0200

    feat: updated 09 lesson

--- src/09/index.ts
+++ src/09/index.ts
@@ -3,26 +3,31 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+const db = {
+    courses: [
+        {id: 1, title: 'front-end'},
+        {id: 2, title: 'back-end'},
+        {id: 3, title: 'full-stack'},
+        {id: 4, title: 'mobile'},
+    ],
+};
+
 app.get('/', (req, res) => {
     res.send({message: 'Hello'});
 });
 
 app.get('/courses', (req, res) => {
-    res.json([
-        {id: 1, name: 'front-end'},
-        {id: 2, name: 'back-end'},
-        {id: 3, name: 'full-stack'},
-        {id: 4, name: 'mobile'},
-    ]);
+    let foundCourses = db.courses;
+
+    if (req.query.title) {
+        foundCourses = foundCourses.filter(course => course.title.indexOf(req.query.title as string) > -1);
+    }
+
+    res.json(foundCourses);
 });
 
 app.get('/courses/:id', (req, res) => {
-    const foundCourse = [
-        {id: 1, name: 'front-end'},
-        {id: 2, name: 'back-end'},
-        {id: 3, name: 'full-stack'},
-        {id: 4, name: 'mobile'},
-    ].find(course => course.id === +req.params.id);
+    const foundCourse = db.courses.find(course => course.id === +req.params.id);
 
     if (!foundCourse) {
         res.sendStatus(404);
