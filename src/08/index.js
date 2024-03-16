64a2d274e23b72ce0c2d721954ce55ac9e3b2a6d
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-03-16 03:06:49 +0200

    feat: added 08 lesson

---
+++ src/08/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/nastya', (req, res) => {
+    res.send('Hello Nastya!')
+})
+
+app.get('/vova', (req, res) => {
+    res.send('Hello Vova!')
+})
+
+app.post('/user', (req, res) => {
+    res.send('User created')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
