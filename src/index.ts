6eb1328347f57721808f881d20d9f07e19e5c497
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-10-01 22:40:56 +0300

    feat: updated 09 lesson

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send({message: 'Hello Nastya!'});
+    res.json({message: 'Hello Nastya!'});
 });
 
 app.listen(port, () => {
