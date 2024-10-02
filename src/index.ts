826fd100596f6cf05ccc3e3eb6ece065011570dd
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-10-02 22:48:33 +0300

    feat: updated 09 lesson

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.json({message: 'Hello Nastya!'});
+    res.sendStatus(404);
+});
+
+app.get('/authors', (req: Request, res: Response) => {
+    res.json([
+        {id: 1, author: 'Stephen King'},
+        {id: 2, author: 'Joe Hill'},
+        {id: 3, author: 'Clive Barker'},
+    ]);
 });
 
 app.listen(port, () => {
