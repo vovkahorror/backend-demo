24f40d443e2724df9999d8254486d2422dfd0b99
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-10-24 00:55:00 +0300

    feat: updated the tenth lesson

--- src/10/index.ts
+++ src/10/index.ts
@@ -1,8 +1,9 @@
-const express = require('express')
+import express from 'express';
+
 const app = express()
 const port = 3000
 
-app.get('/', (req: any, res: any) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
