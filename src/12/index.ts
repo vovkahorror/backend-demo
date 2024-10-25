5d5ecb9968a69d62a38b73eb6fce0b48d811111f
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-10-26 00:45:00 +0300

    feat: updated the 12 lesson

--- src/12/index.ts
+++ src/12/index.ts
@@ -3,3 +3,7 @@ import express from 'express'
 const app = express()
 
 const port = process.env.PORT || 5000
+
+const products = [{title: 'apple'}, {title: 'potato'}]
+const addresses = [{value: 'Kherson'}, {title: 'Kyiv'}, {title: 'Ivano-Frankivsk'}]
+
