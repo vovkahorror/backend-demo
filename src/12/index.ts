97b1d1034d4c9aea3fa913b457c3c5bba2255899
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-10-27 00:46:00 +0300

    feat: updated the 12 lesson

--- src/12/index.ts
+++ src/12/index.ts
@@ -7,3 +7,14 @@ const port = process.env.PORT || 5000
 const products = [{title: 'apple'}, {title: 'potato'}]
 const addresses = [{value: 'Kherson'}, {title: 'Kyiv'}, {title: 'Ivano-Frankivsk'}]
 
+app.get('/products', (req, res) => {
+    res.send(products)
+})
+
+app.get('/addresses', (req, res) => {
+    res.send(addresses)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
