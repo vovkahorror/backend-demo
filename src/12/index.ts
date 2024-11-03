d3f172ec1229ecf5b3f1d8eed4acf716d72e61da
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-11-04 01:25:33 +0200

    feat: updated the 12 task

--- src/12/index.ts
+++ src/12/index.ts
@@ -1,46 +1,58 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-const products = [{id: 1, title: 'apple'}, {id: 2, title: 'potato'}]
-const addresses = [{id: 1, value: 'Kherson'}, {id: 2, title: 'Kyiv'}, {id: 3, title: 'Ivano-Frankivsk'}]
+const products = [{id: 1, title: 'apple'}, {id: 2, title: 'potato'}];
+const addresses = [{id: 1, value: 'Kherson'}, {id: 2, title: 'Kyiv'}, {id: 3, title: 'Ivano-Frankivsk'}];
 
 app.get('/products', (req, res) => {
     if (req.query.title) {
-        const searchString = req.query.title.toString()
-        const filteredProducts = products.filter(p => p.title.includes(searchString))
-        res.send(filteredProducts)
+        const searchString = req.query.title.toString();
+        const filteredProducts = products.filter(p => p.title.includes(searchString));
+        res.send(filteredProducts);
     } else {
-        res.send(products)
+        res.send(products);
     }
-})
+});
 
 app.get('/products/:productTitle', (req, res) => {
-    const product = products.find(p => p.title === req.params.productTitle)
+    const product = products.find(p => p.title === req.params.productTitle);
 
     if (product) {
-        res.send(product)
+        res.send(product);
     } else {
-        res.send(404)
+        res.send(404);
     }
-})
+});
+
+app.delete('/products/:id', (req, res) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1);
+            res.send(204);
+            return;
+        }
+    }
+
+    res.send(404);
+});
 
 app.get('/addresses', (req, res) => {
-    res.send(addresses)
-})
+    res.send(addresses);
+});
 
 app.get('/addresses/:id', (req, res) => {
-    const address = addresses.find(a => a.id === +req.params.id)
+    const address = addresses.find(a => a.id === +req.params.id);
 
     if (address) {
-        res.send(address)
+        res.send(address);
     } else {
-        res.send(404)
+        res.send(404);
     }
-})
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
