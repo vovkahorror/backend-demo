b31ba64e4075c2a80e2b627fd483551d5a779d93
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-11-06 02:52:50 +0200

    feat: updated the 12 task

--- src/12/index.ts
+++ src/12/index.ts
@@ -40,6 +40,17 @@ app.get('/products/:productTitle', (req, res) => {
     }
 });
 
+app.put('/products/:id', (req, res) => {
+    const product = products.find(p => p.id === +req.params.id);
+
+    if (product) {
+        product.title = req.body.title;
+        res.send(product);
+    } else {
+        res.send(404);
+    }
+});
+
 app.delete('/products/:id', (req, res) => {
     for (let i = 0; i < products.length; i++) {
         if (products[i].id === +req.params.id) {
