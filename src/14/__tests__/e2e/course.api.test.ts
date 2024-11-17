b16bbe75395e07ee50ee920fdf6f288ee3790d14
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-11-18 01:51:08 +0200

    feat: updated the 14th task

--- src/14/__tests__/e2e/course.api.test.ts
+++ src/14/__tests__/e2e/course.api.test.ts
@@ -29,15 +29,41 @@ describe('/course', () => {
             expect(HTTP_STATUSES.OK_200, []);
     })
 
+    let createdCourse: any = null;
+
     it('should create course with correct input data', async () => {
         const createdResponse = await request(app).
             post('/courses').
             send({title: 'turbo pascal'}).
             expect(HTTP_STATUSES.CREATED_201);
 
-        expect(createdResponse.body).toEqual({
+        createdCourse = createdResponse.body;
+
+        expect(createdCourse).toEqual({
             id: expect.any(Number),
             title: 'turbo pascal'
         })
+
+        await request(app).
+        get('/courses').
+        expect(HTTP_STATUSES.OK_200, [createdCourse]);
+    })
+
+    it('should\'nt update course with incorrect input data', async () => {
+        await request(app).
+            put(`/courses/${createdCourse.id}`).
+            send({title: ''}).
+            expect(HTTP_STATUSES.BAD_REQUEST_400);
+
+        await request(app).
+            get('/courses').
+            expect(HTTP_STATUSES.OK_200, [createdCourse]);
+    })
+
+    it('should\'nt update not existing course', async () => {
+        await request(app).
+            put('/courses/2').
+            send({title: 'updated course'}).
+            expect(HTTP_STATUSES.NOT_FOUND_404);
     })
 })
