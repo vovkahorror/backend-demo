a61d2515767c8fa45441dedc3a6bcdcf45c45313
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-12-08 02:11:39 +0200

    feat: added tests to the 15th task

---
+++ src/15/__tests__/e2e/course.api.test.ts
@@ -0,0 +1,95 @@
+import request from 'supertest'
+import {app, HTTP_STATUSES} from '../..';
+import {CreateCourseModel} from '../../models/CreateCourseModel';
+import {UpdateCourseModel} from '../../models/UpdateCourseModel';
+
+describe('/course', () => {
+    beforeAll(async () => {
+        await request(app).delete('/__test__/data')
+    })
+
+    it('should return 200 and empty array',  async() => {
+        await request(app).
+            get('/courses').
+            expect(HTTP_STATUSES.OK_200, []);
+    })
+
+    it('should return 404 for not existing course', async () => {
+        await request(app).
+            get('/courses/1').
+            expect(HTTP_STATUSES.NOT_FOUND_404);
+    })
+
+    it('should\'nt create course with incorrect input data', async () => {
+        const data: CreateCourseModel = {title: ''};
+
+        await request(app).
+            post('/courses').
+            send(data).
+            expect(HTTP_STATUSES.BAD_REQUEST_400);
+
+        await request(app).
+            get('/courses').
+            expect(HTTP_STATUSES.OK_200, []);
+    })
+
+    let createdCourse: any = null;
+
+    it('should create course with correct input data', async () => {
+        const data: CreateCourseModel = {title: 'turbo pascal'};
+
+        const createdResponse = await request(app).
+            post('/courses').
+            send(data).
+            expect(HTTP_STATUSES.CREATED_201);
+
+        createdCourse = createdResponse.body;
+
+        expect(createdCourse).toEqual({
+            id: expect.any(Number),
+            title: data.title
+        })
+
+        await request(app).
+        get('/courses').
+        expect(HTTP_STATUSES.OK_200, [createdCourse]);
+    })
+
+    it('should\'nt update course with incorrect input data', async () => {
+        const data: UpdateCourseModel = {title: ''};
+
+        await request(app).
+            put(`/courses/${createdCourse.id}`).
+            send(data).
+            expect(HTTP_STATUSES.BAD_REQUEST_400);
+
+        await request(app).
+            get('/courses').
+            expect(HTTP_STATUSES.OK_200, [createdCourse]);
+    })
+
+    it('should\'nt update not existing course', async () => {
+        const data: UpdateCourseModel = {title: 'updated course'};
+
+        await request(app).
+            put('/courses/-100').
+            send(data).
+            expect(HTTP_STATUSES.NOT_FOUND_404);
+    })
+
+    it('should update course with correct input data', async () => {
+        const data: UpdateCourseModel = {title: 'updated course'};
+
+        await request(app).
+            put(`/courses/${createdCourse.id}`).
+            send(data).
+            expect(HTTP_STATUSES.OK_200);
+
+        await request(app).
+            get('/courses').
+            expect(HTTP_STATUSES.OK_200, [{
+                ...createdCourse,
+                title: data.title
+            }]);
+    })
+})
