a71769d60a933a236cb66da7a536462cd37f6a6a
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-12-28 00:58:21 +0200

    refactor: naming

--- src/16/routes/main.ts
+++ src/16/routes/main.ts
@@ -2,15 +2,15 @@ import express from 'express';
 import {HTTP_STATUSES} from '../common/enums/http-statuses';
 
 export const getMainRouter = () => {
-    const coursesRouter = express.Router();
+    const mainRouter = express.Router();
 
-    coursesRouter.get('/', (req, res) => {
+    mainRouter.get('/', (req, res) => {
         res.send({message: 'Hello'});
     });
 
-    coursesRouter.get('/status', (req, res) => {
+    mainRouter.get('/status', (req, res) => {
         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
     });
 
-    return coursesRouter
+    return mainRouter
 };
