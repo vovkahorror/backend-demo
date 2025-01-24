a53d5cf42ab680d77c09f9f073cb994aa43aa858
Volodymyr Yaremchak <vovkahorror@gmail.com>
2025-01-25 01:24:29 +0200

    fix: updated the 20th task

--- src/20/index.ts
+++ src/20/index.ts
@@ -7,7 +7,7 @@ app.use(express.json())
 
 const port = process.env.PORT || 5000
 
-app.use('products', productsRouter)
+app.use('/products', productsRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
