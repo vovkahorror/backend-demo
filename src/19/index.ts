ecc78c0b79e5818ac42bc65c0321c61b57081d67
Volodymyr Yaremchak <vovkahorror@gmail.com>
2025-06-16 01:32:51 +0300

    feat: updated the 19th task

--- src/19/index.ts
+++ src/19/index.ts
@@ -4,11 +4,18 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+let requestCounter = 0;
+
+const requestCounterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    requestCounter++;
+    next();
+};
+
 const blablaMiddleware = (req: Request, res: Response, next: NextFunction) => {
     // @ts-ignore
     req.blalbla = 'hello';
     next();
-}
+};
 
 const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
     if (req.query.token === '123') {
@@ -16,9 +23,9 @@ const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) =>
     } else {
         res.sendStatus(401);
     }
-}
+};
 
-app.use(blablaMiddleware, authGuardMiddleware)
+app.use(requestCounterMiddleware, blablaMiddleware, authGuardMiddleware);
 
 app.get('/products', (req, res) => {
     // @ts-ignore
